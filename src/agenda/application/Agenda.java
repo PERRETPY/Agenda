7d6694931d4694ceb5d53facdeddc0f0e594ff5c
perretpy <pierreyves.perret@gmail.com>
2022-03-04 08:34:13 +0100

    Add application class

---
+++ src/agenda/application/Agenda.java
@@ -0,0 +1,15 @@
+package agenda.application;
+
+public class Agenda implements Runnable {
+
+    public Agenda() {
+
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello world!");
+    }
+
+
+}
